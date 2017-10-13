6478990749d26e61b5414209c56ffd8765ded75d
lb <it_liubin@163.com>
2017-10-13 14:56:46 +0800

    up

---
+++ lb-blog/src/main/java/lb_blog/lb_blog/App.java
@@ -0,0 +1,13 @@
+package lb_blog.lb_blog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
